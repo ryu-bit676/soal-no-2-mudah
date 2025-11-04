fa988504bbf52061c9571e57efc69df4dbf45b18
ryu-bit676 <achmad_mariadi_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 16:17:49 +0700

    Add files via upload

---
+++ soal2_mudah.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class soal2_mudah {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("masukan angka :");
+        int angka = input.nextInt();
+        if (angka % 2 == 0){
+            System.out.println(angka +" adalah bilangan genap");
+        }else if (angka % 2 != 0){
+            System.out.println(angka + " adalah bilangan ganjil");
+
+        }
+
+        }
+        }
+
+
+        
+    
+    
+
